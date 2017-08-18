4f0fe6e7ffe8c2e4ef08e7a106991305b39bf793
Willin Wang <willin@willin.org>
2017-08-18 10:58:36 +0800

    pm2 config

--- server/coding.config.js
+++ server/coding.config.js
@@ -4,12 +4,12 @@ const path = require('path');
 module.exports = {
   apps: [
     {
-      name: 'coding-graph',
+      name: 'coding-api',
       script: 'index.js',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      cwd: path.join(__dirname, './server'),
-      error_file: path.join(__dirname, '../log/coding-graph.err.log'),
-      out_file: path.join(__dirname, '../log/coding-graph.out.log'),
+      cwd: path.join(__dirname, './'),
+      error_file: path.join(__dirname, '../../log/coding-graph.err.log'),
+      out_file: path.join(__dirname, '../../log/coding-graph.out.log'),
       // instances: 0,
       // exec_mode: 'cluster',
       max_memory_restart: '800M',
@@ -19,9 +19,9 @@ module.exports = {
       name: 'coding-task',
       script: 'index.js',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      cwd: path.join(__dirname, './server/cron'),
-      error_file: path.join(__dirname, '../log/coding-task.err.log'),
-      out_file: path.join(__dirname, '../log/coding-task.out.log'),
+      cwd: path.join(__dirname, './cron'),
+      error_file: path.join(__dirname, '../../log/coding-task.err.log'),
+      out_file: path.join(__dirname, '../../log/coding-task.out.log'),
       max_memory_restart: '800M',
       merge_logs: true
     }

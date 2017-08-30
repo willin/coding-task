3ee4e8dc3bf5bdad28016642bde02ec0b9eecd19
Willin Wang <willin@willin.org>
2017-08-30 18:17:24 +0800

    fix user graph bug

--- src/router/statistics/user.vue
+++ src/router/statistics/user.vue
@@ -24,354 +24,352 @@
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
-import 'echarts/lib/component/legend'
+import { mapGetters } from 'vuex';
+import 'echarts/lib/component/legend';
 import 'echarts/lib/chart/graph';
 
 import chart from '../../component/chart';
 
-  export default {
-    props: ['params'],
-    components: {
-      chart
-    },
-    data() {
+export default {
+  props: ['params'],
+  components: {
+    chart
+  },
+  data() {
+    return {
+      yAxisData: [],
+      seriesData: [[], []]
+    };
+  },
+  created() {
+    for (let i = 0; i < this.users.length; i += 1) {
+      this.yAxisData.push(this.users[i].name);
+      this.seriesData[0].push(this.tasks.filter(x => x.owner_id === this.users[i].id && x.status === 1).length); // 未完成
+      this.seriesData[1].push(this.tasks.filter(x => x.owner_id === this.users[i].id && x.status === 2).length); // 已完成
+    }
+  },
+  methods: {
+    userTask(user) {
       return {
-        yAxisData: [],
-        seriesData: [[], []]
+        title: {
+          show: false
+        },
+        tooltip: {
+          show: false
+        },
+        silent: true,
+        animationDurationUpdate: 1500,
+        animationEasingUpdate: 'quinticInOut',
+        series: [
+          {
+            type: 'graph',
+            layout: 'none',
+            legendHoverLink: false,
+            symbolSize: 100,
+            roam: false,
+            label: {
+              normal: {
+                show: true,
+                fontSize: 12
+              }
+            },
+            edgeSymbol: ['none', 'none'],
+            edgeSymbolSize: [4, 10],
+            data: [
+              {
+                name: `分配任务数\n${this.tasks.filter(x => x.owner_id === user.id).length}`,
+                x: 200,
+                y: 300,
+                itemStyle: {
+                  normal: {
+                    color: '#f4ab0f'
+                  }
+                }
+              },
+              {
+                name: `创建任务数\n${this.tasks.filter(x => x.creator_id === user.id).length}`,
+                x: 0,
+                y: 300,
+                itemStyle: {
+                  normal: {
+                    color: '#42a5f5'
+                  }
+                }
+              },
+              {
+                name: `已完成任务数\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 2).length}`,
+                x: 400,
+                y: 120,
+                itemStyle: {
+                  normal: {
+                    color: '#25cc3d'
+                  }
+                }
+              },
+              {
+                name: `未完成任务数\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1).length}`,
+                x: 450,
+                y: 380,
+                itemStyle: {
+                  normal: {
+                    color: '#ff8786'
+                  }
+                }
+              },
+              {
+                name: `重要且紧急任务\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1 && x.priority === 3).length}`,
+                x: 640,
+                y: 200,
+                itemStyle: {
+                  normal: {
+                    color: '#ff0f07'
+                  }
+                }
+              },
+              {
+                name: `重要不紧急任务\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1 && x.priority === 2).length}`,
+                x: 740,
+                y: 310,
+                itemStyle: {
+                  normal: {
+                    color: '#f540ed'
+                  }
+                }
+              },
+              {
+                name: `紧急不重要任务\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1 && x.priority === 1).length}`,
+                x: 700,
+                y: 420,
+                itemStyle: {
+                  normal: {
+                    color: '#fb3ca3'
+                  }
+                }
+              },
+              {
+                name: `不紧急不重要任务\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1 && x.priority === 0).length}`,
+                x: 600,
+                y: 520,
+                itemStyle: {
+                  normal: {
+                    color: '#aba3e1'
+                  }
+                }
+              }
+            ],
+            links: [
+              {
+                source: 0,
+                target: 1
+              },
+              {
+                source: 0,
+                target: 2
+              },
+              {
+                source: 0,
+                target: 3
+              },
+              {
+                source: 3,
+                target: 4
+              },
+              {
+                source: 3,
+                target: 5
+              },
+              {
+                source: 3,
+                target: 6
+              },
+              {
+                source: 3,
+                target: 7
+              }
+            ],
+            lineStyle: {
+              normal: {
+                opacity: 0.9,
+                width: 2,
+                curveness: 0
+              }
+            }
+          }
+        ]
       };
     },
-    created: function () {
-      for (let i in this.users) {
-        this.yAxisData.push(this.users[i].name);
-        this.seriesData[0].push(this.tasks.filter(x => x.owner_id === this.users[i].id && x.status === 1).length); //未完成
-        this.seriesData[1].push(this.tasks.filter(x => x.owner_id === this.users[i].id && x.status === 2).length); //已完成
-      }
-    },
-    methods: {
-      userTask(user) {
-        return {
-          title: {
+    allUserTask() {
+      return {
+        legend: {
+          data: ['未完成', '已完成'],
+          top: 35,
+          itemGap: 120
+        },
+        grid: [{
+          show: false,
+          left: 0,
+          containLabel: false,
+          width: '46%'
+        }, {
+          show: false,
+          left: '51%',
+          containLabel: false,
+          width: '8%'
+        }, {
+          show: false,
+          right: 0,
+          containLabel: false,
+          width: '46%'
+        }],
+        xAxis: [{
+          show: false,
+          type: 'value',
+          inverse: true,
+          axisLine: {
             show: false
           },
-          tooltip: {
+          axisTick: {
             show: false
           },
-          silent: true,
-          animationDurationUpdate: 1500,
-          animationEasingUpdate: 'quinticInOut',
-          series: [
-            {
-              type: 'graph',
-              layout: 'none',
-              legendHoverLink: false,
-              symbolSize: 100,
-              roam: false,
-              label: {
-                normal: {
-                  show: true,
-                  fontSize: 12
-                }
-              },
-              edgeSymbol: ['none', 'none'],
-              edgeSymbolSize: [4, 10],
-              data: [
-                {
-                  name: `分配任务数\n${this.tasks.filter(x => x.owner_id === user.id).length}`,
-                  x: 200,
-                  y: 300,
-                  itemStyle: {
-                    normal: {
-                      color: '#f4ab0f'
-                    }
-                  }
-                },
-                {
-                  name: `创建任务数\n${this.tasks.filter(x => x.creator_id === user.id).length}`,
-                  x: 0,
-                  y: 300,
-                  itemStyle: {
-                    normal: {
-                      color: '#42a5f5'
-                    }
-                  }
-                },
-                {
-                  name: `已完成任务数\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 2).length}`,
-                  x: 400,
-                  y: 120,
-                  itemStyle: {
-                    normal: {
-                      color: '#25cc3d'
-                    }
-                  }
-                },
-                {
-                  name: `未完成任务数\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1).length}`,
-                  x: 450,
-                  y: 380,
-                  itemStyle: {
-                    normal: {
-                      color: '#ff8786'
-                    }
-                  }
-                },
-                {
-                  name: `重要且紧急任务\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1 && x.priority === 3).length}`,
-                  x: 640,
-                  y: 200,
-                  itemStyle: {
-                    normal: {
-                      color: '#ff0f07'
-                    }
-                  }
-                },
-                {
-                  name: `重要不紧急任务\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1 && x.priority === 2).length}`,
-                  x: 740,
-                  y: 310,
-                  itemStyle: {
-                    normal: {
-                      color: '#f540ed'
-                    }
-                  }
-                },
-                {
-                  name: `紧急不重要任务\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1 && x.priority === 1).length}`,
-                  x: 700,
-                  y: 420,
-                  itemStyle: {
-                    normal: {
-                      color: '#fb3ca3'
-                    }
-                  }
-                },
-                {
-                  name: `不紧急不重要任务\n${this.tasks.filter(x => x.owner_id === user.id && x.status === 1 && x.priority === 0).length}`,
-                  x: 600,
-                  y: 520,
-                  itemStyle: {
-                    normal: {
-                      color: '#aba3e1'
-                    }
-                  }
-                }
-              ],
-              links: [
-                {
-                  source: 0,
-                  target: 1
-                },
-                {
-                  source: 0,
-                  target: 2
-                },
-                {
-                  source: 0,
-                  target: 3
-                },
-                {
-                  source: 3,
-                  target: 4
-                },
-                {
-                  source: 3,
-                  target: 5
-                },
-                {
-                  source: 3,
-                  target: 6
-                },
-                {
-                  source: 3,
-                  target: 7
-                }
-              ],
-              lineStyle: {
-                normal: {
-                  opacity: 0.9,
-                  width: 2,
-                  curveness: 0
-                }
-              }
-            }
-          ]
-        };
-      },
-      allUserTask(){
-        return {
-          legend: {
-            data: ['未完成', '已完成'],
-            top: 35,
-            itemGap: 120
-          },
-          grid: [{
-            show: false,
-            left: 0,
-            containLabel: false,
-            width: '46%'
-          }, {
-            show: false,
-            left: '51%',
-            containLabel: false,
-            width: '8%'
-          }, {
-            show: false,
-            right: 0,
-            containLabel: false,
-            width: '46%'
-          }],
-          xAxis: [{
-            show: false,
-            type: 'value',
-            inverse: true,
-            axisLine: {
-              show: false
-            },
-            axisTick: {
-              show: false
-            },
-            position: 'bottom',
-            axisLabel: {
-              show: false
-            },
-            splitLine: {
-              show: false
-            }
-          }, {
-            gridIndex: 1,
+          position: 'bottom',
+          axisLabel: {
             show: false
-          }, {
-            gridIndex: 2,
-            type: 'value',
-            axisLine: {
-              show: false
-            },
-            axisTick: {
-              show: false
-            },
-            position: 'bottom',
-            axisLabel: {
-              show: false
-            },
-            splitLine: {
-              show: false
-            }
-          }],
-          yAxis: [{
-            type: 'category',
-            inverse: true,
-            position: 'right',
-            axisLine: {
-              show: false
-            },
-            axisTick: {
-              show: false
-            },
-            axisLabel: {
-              show: false
-            },
-            data: []
+          },
+          splitLine: {
+            show: false
+          }
+        }, {
+          gridIndex: 1,
+          show: false
+        }, {
+          gridIndex: 2,
+          type: 'value',
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          position: 'bottom',
+          axisLabel: {
+            show: false
+          },
+          splitLine: {
+            show: false
+          }
+        }],
+        yAxis: [{
+          type: 'category',
+          inverse: true,
+          position: 'right',
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          axisLabel: {
+            show: false
+          },
+          data: []
 
-          }, {
-            gridIndex: 1,
-            type: 'category',
-            inverse: true,
-            position: 'left',
-            axisLine: {
-              show: false
-            },
-            axisTick: {
-              show: false
-            },
-            axisLabel: {
-              show: true
-            },
-            data: this.yAxisData.map(function (value) {
-              return {
-                value: value,
+        }, {
+          gridIndex: 1,
+          type: 'category',
+          inverse: true,
+          position: 'left',
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          axisLabel: {
+            show: true
+          },
+          data: this.yAxisData.map(value => ({
+            value,
+            textStyle: {
+              align: 'center',
+              color: '#000',
+              fontSize: 12
+            }
+          }))
+        }, {
+          gridIndex: 2,
+          type: 'category',
+          inverse: true,
+          position: 'left',
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          axisLabel: {
+            show: false
+          },
+          data: []
+        }],
+        series: [
+          {
+            name: '未完成',
+            type: 'bar',
+            barWidth: 20,
+            label: {
+              normal: {
+                show: true,
+                color: 'red',
+                position: 'insideLeft',
                 textStyle: {
-                  align: 'center',
-                  color: '#000',
-                  fontSize: 12
+                  color: '#fff'
                 }
               }
-            })
-          }, {
-            gridIndex: 2,
-            type: 'category',
-            inverse: true,
-            position: 'left',
-            axisLine: {
-              show: false
             },
-            axisTick: {
-              show: false
+            itemStyle: {
+              normal: {
+                color: '#ff5d5d',
+                barBorderRadius: [5, 0, 0, 5]
+              }
             },
-            axisLabel: {
-              show: false
+            data: this.seriesData[0]
+          },
+          {
+            name: '已完成',
+            type: 'bar',
+            barWidth: 20,
+            xAxisIndex: 2,
+            yAxisIndex: 2,
+            label: {
+              normal: {
+                show: true,
+                color: 'red',
+                position: 'insideRight',
+                textStyle: {
+                  color: '#ffffff'
+                }
+              }
             },
-            data: []
-          }],
-          series: [
-            {
-              name: '未完成',
-              type: 'bar',
-              barWidth: 20,
-              label: {
-                normal: {
-                  show: true,
-                  color: 'red',
-                  position: 'insideLeft',
-                  textStyle: {
-                    color: '#fff'
-                  }
-                }
+            itemStyle: {
+              normal: {
+                color: '#00cc99',
+                barBorderRadius: [0, 5, 5, 0]
               },
-              itemStyle: {
-                normal: {
-                  color: '#ff5d5d',
-                  barBorderRadius: [5, 0, 0, 5]
-                }
-              },
-              data: this.seriesData[0]
+              emphasis: {
+                show: false
+              }
             },
-            {
-              name: '已完成',
-              type: 'bar',
-              barWidth: 20,
-              xAxisIndex: 2,
-              yAxisIndex: 2,
-              label: {
-                normal: {
-                  show: true,
-                  color: 'red',
-                  position: 'insideRight',
-                  textStyle: {
-                    color: '#ffffff'
-                  }
-                }
-              },
-              itemStyle: {
-                normal: {
-                  color: '#00cc99',
-                  barBorderRadius: [0, 5, 5, 0]
-                },
-                emphasis: {
-                  show: false
-                }
-              },
-              data: this.seriesData[1]
-            }
-          ]
-        };
-      }
-    },
-    computed: {
-      ...mapGetters([
-        'users',
-        'tasks'
-      ])
+            data: this.seriesData[1]
+          }
+        ]
+      };
     }
-  };
+  },
+  computed: {
+    ...mapGetters([
+      'users',
+      'tasks'
+    ])
+  }
+};
 </script>

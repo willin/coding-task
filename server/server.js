3607f83c08a0369940e7e094ce70e82b5f729d7f
Willin Wang <willin@willin.org>
2017-08-31 12:12:11 +0800

    fix f gpa file

--- server/server.js
+++ server/server.js
@@ -7,123 +7,103 @@ const { setToken } = require('./lib/token');
 
 const app = new Koa();
 const router = new Router();
-const modelOptions = {
-  raw: true
-};
 
-router.get('/coding/callback', async (ctx) => {
-  const code = ctx.query.code;
-  const token = await getAccessToken({ code });
-  if (token === undefined) {
-    ctx.redirect('/login?result=500');
-    return;
-  }
-  // 判断是否能够登录
-  const team = await getTeams({ accessToken: token.access_token });
-  let canLogin = false;
-  for (let i = 0; i < team.length; i += 1) {
-    if (teams.includes(team[i].global_key)) {
-      canLogin = true;
-      break;
+router
+  .get('/coding/callback', async (ctx) => {
+    const code = ctx.query.code;
+    const token = await getAccessToken({ code });
+    if (token === undefined) {
+      ctx.redirect('/login?result=500');
+      return;
     }
-  }
-  if (canLogin) {
+    // 判断是否能够登录
+    const team = await getTeams({ accessToken: token.access_token });
+    let canLogin = false;
+    for (let i = 0; i < team.length; i += 1) {
+      if (teams.includes(team[i].global_key)) {
+        canLogin = true;
+        break;
+      }
+    }
+    if (canLogin) {
     // 获取用户名
-    const user = await getCurrentUser({ accessToken: token.access_token });
-    if (user.global_key === admin) {
-      await setToken(token);
+      const user = await getCurrentUser({ accessToken: token.access_token });
+      if (user.global_key === admin) {
+        await setToken(token);
+      }
+      // 登录成功跳转
+      ctx.redirect(`/login?result=${token.access_token}`);
+      return;
     }
-    // 登录成功跳转
-    ctx.redirect(`/login?result=${token.access_token}`);
-    return;
-  }
-  // 登录失败跳转
-  ctx.redirect('/login?result=fail');
-});
+    // 登录失败跳转
+    ctx.redirect('/login?result=fail');
+  })
 
-router.get('/check', async (ctx) => {
-  const token = ctx.query.token;
-  const result = await getCurrentUser({ accessToken: token });
-  ctx.body = { status: result === undefined ? 0 : 1 };
-});
+  .get('/check', async (ctx) => {
+    const token = ctx.query.token;
+    const result = await getCurrentUser({ accessToken: token });
+    ctx.body = { status: result === undefined ? 0 : 1 };
+  })
 
-router.get('/user', async (ctx) => {
-  const data = await User.findAll(modelOptions);
-  ctx.body = {
-    status: 1,
-    data
-  };
-});
+  .get('/user', async (ctx) => {
+    const data = await User.findAll({
+      raw: true
+    });
+    ctx.body = {
+      status: 1,
+      data
+    };
+  })
 
-router.get('/team', async (ctx) => {
-  const data = await Team.findAll(modelOptions);
-  ctx.body = {
-    status: 1,
-    data
-  };
-});
+  .get('/team', async (ctx) => {
+    const data = await Team.findAll({
+      raw: true
+    });
+    ctx.body = {
+      status: 1,
+      data
+    };
+  })
 
-router.get('/project', async (ctx) => {
-  const data = await Project.findAll(modelOptions);
-  ctx.body = {
-    status: 1,
-    data
-  };
-});
+  .get('/project', async (ctx) => {
+    const data = await Project.findAll({
+      raw: true
+    });
+    ctx.body = {
+      status: 1,
+      data
+    };
+  })
 
-router.get('/task', async (ctx) => {
-  const data = await Task.findAll(modelOptions);
-  ctx.body = {
-    status: 1,
-    data
-  };
-});
+  .get('/task', async (ctx) => {
+    const data = await Task.findAll({
+      raw: true
+    });
+    ctx.body = {
+      status: 1,
+      data
+    };
+  })
 
-router.get('/label', async (ctx) => {
-  const data = await Label.findAll(modelOptions);
-  ctx.body = {
-    status: 1,
-    data
-  };
-});
+  .get('/label', async (ctx) => {
+    const data = await Label.findAll({
+      raw: true
+    });
+    ctx.body = {
+      status: 1,
+      data
+    };
+  })
 
-router.get('/tasklabels', async (ctx) => {
-  const data = await TaskLabels.findAll(modelOptions);
-  ctx.body = {
-    status: 1,
-    data
-  };
-});
-
-// 示例代码: 供后续优化拆分参考, 但这样的两个接口设计会产生大量冗余请求. 需要重新设计接口及参数.
-// router.get('/task/:taskid/labels', async (ctx) => {
-//   const taskid = ctx.params.taskid;
-//   const data = await TaskLabels.findAll({
-//     where: {
-//       task_id: taskid
-//     },
-//     raw: true
-//   });
-//   ctx.body = {
-//     status: 1,
-//     data: data.map(x => x.label_id)
-//   };
-// });
-
-// router.get('/label/:labelid/tasks', async (ctx) => {
-//   const labelid = ctx.params.labelid;
-//   const data = await TaskLabels.findAll({
-//     where: {
-//       label_id: labelid
-//     },
-//     raw: true
-//   });
-//   ctx.body = {
-//     status: 1,
-//     data: data.map(x => x.task_id)
-//   };
-// });
-
+  .get('/tasklabels', async (ctx) => {
+    const data = await TaskLabels.findAll({
+      raw: true
+    });
+    ctx.body = {
+      status: 1,
+      data
+    };
+  });
 
 app.use(router.routes()).use(router.allowedMethods());
 

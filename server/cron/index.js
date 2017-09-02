64db3d8f36f66a55b02541b79176e982f65b29fe
Willin Wang <willin@willin.org>
2017-09-02 19:56:19 +0800

    fix notice

--- server/cron/index.js
+++ server/cron/index.js
@@ -2,7 +2,7 @@ const { defaults: { type }, notification: { type: notification } } = require('..
 const CronJob = require('cron').CronJob;
 const sync = require('./sync');
 const { refreshToken } = require('./token');
-const { dailyNotice, weeklyNotice } = require('./notify');
+const { dailyNotice, notice } = require('./notify');
 
 /* eslint-disable no-new */
 // 更新任务数据, 每隔10分定时执行
@@ -21,10 +21,10 @@ if (notification) {
   new CronJob('0 30 8 * * 1-5', async () => {
     await dailyNotice();
   }, null, true, 'Asia/Shanghai');
-  new CronJob('0 0 14 * * 6', async () => {
-    await weeklyNotice();
+  new CronJob('0 0 20 * * 6', async () => {
+    await notice('week');
   }, null, true, 'Asia/Shanghai');
   new CronJob('0 0 9 1 * *', async () => {
-    await weeklyNotice();
+    await notice('month');
   }, null, true, 'Asia/Shanghai');
 }

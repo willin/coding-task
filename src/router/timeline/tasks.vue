9f5e90551d1c70514924cf420b53e33ff0ec179d
yp <924083087@qq.com>
2017-09-04 14:17:06 +0800

    调试时间显示

--- src/router/timeline/tasks.vue
+++ src/router/timeline/tasks.vue
@@ -65,6 +65,11 @@ export default {
   data() {
     return {};
   },
+  created() {
+    console.log(this.tasks);
+    console.log('this.tasks[0].deadline= ' + this.tasks[0].deadline);
+    console.log('moment(this.tasks[0].deadline).format(\'YYYY年\') =' + moment(this.tasks[0].deadline).format('YYYY年'));
+  },
   methods: {
     moment,
     displayPriority(i) {
